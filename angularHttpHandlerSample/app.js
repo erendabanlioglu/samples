950a40b2389acd3af54e16a333ac4b9527e718b3
Eren D <edabanlioglu@gmail.com>
2015-11-20 10:16:58 +0100

    Create app.js

---
+++ angularHttpHandlerSample/app.js
@@ -0,0 +1,11 @@
+ .config(function ($httpProvider, httpRequestInterceptorCacheBusterProvider) {
+
+        //Cache everything except rest api requests
+        httpRequestInterceptorCacheBusterProvider.setMatchlist([/.*api.*/, /.*protected.*/], true);
+
+        $httpProvider.interceptors.push('errorHandlerInterceptor');
+        $httpProvider.interceptors.push('authExpiredInterceptor');
+        $httpProvider.interceptors.push('authInterceptor');
+        $httpProvider.interceptors.push('notificationInterceptor');
+
+    });
